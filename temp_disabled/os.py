c64c525b97912c65ad954cf96221532bbd702f87
HackerAndCoder <100743043+HackerAndCoder@users.noreply.github.com>
2023-07-10 11:24:10 -0600

    Delete temp_disabled directory

--- temp_disabled/os.py
+++
@@ -1,9 +0,0 @@
-import system_app_class, actions
-
-class OS(system_app_class.SystemApp):
-    def __init__(self, local_name='os'):
-        super().__init__(local_name)
-    
-    def get_result(self, action, data={}):
-        return super().get_result(action, data)
-    
